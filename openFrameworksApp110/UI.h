4bac603fd07776a0412e79d091223359f5dab2cb
UjeenDanko <ujeendanko@gmail.com>
2021-05-31 14:43:11 +0100

    m: rebase class to h and fix sound in videoPlayer

---
+++ openFrameworksApp110/UI.h
@@ -0,0 +1,3 @@
+#pragma once
+#include "ofApp.h"
+#include <string> 
--- openFrameworksApp110/src/ofApp.cpp
+++ openFrameworksApp110/src/ofApp.cpp
@@ -2,45 +2,7 @@
 #include <string> 
 
 namespace fs = std::filesystem;
-
-class MediaViewer
-{
-public:
-	ofVideoPlayer videoPlayer;
-	ofImage imagePlayer;
-
-	int startX;
-	int startY;
-
-	int x;
-	int y;
-
-	int maxHeight;
-	int maxWidth;
-
-	int height;
-	int width;
-
-	bool isImageNow = true;
-
-	MediaViewer()
-	{
-
-	}
-
-	MediaViewer(int x_, int y_, int width_, int height_)
-	{
-		startX = x_;
-		startY = y_;
-		x = x_;
-		y = y_;
-		maxHeight = height_;
-		maxWidth = width_;
-		height = height_;
-		width = width_;
-	}
-};
-
+fs::path baseXmlPath = fs::current_path() / "Xml";
 MediaViewer leftViewer;
 MediaViewer rightViewer;
 
@@ -49,114 +11,6 @@ int y = 0;
 int p = 200;
 
 bool isMousePressed = false;
-
-fs::path baseXmlPath = fs::current_path() / "Xml";
-
-enum ComponentType
-{
-	UNKNOWN = 0,
-	VIDEO,
-	IMAGE,
-	FILTER
-};
-
-class MetaData
-{
-public:
-	ofXml xmlRoot;
-	ofVec2f size;
-	ComponentType type;
-	fs::path contentPath;
-	fs::path xmlPath;
-};
-
-class Component
-{
-public:
-	ofxDatGuiComponent* component;
-	ofImage image;
-
-	int startPosX;
-	int startPosY;
-
-	int currentPosX;
-	int currentPosY;
-
-	int height;
-	int width;
-
-	ComponentType type;
-
-	string path = "";
-
-	MetaData metaData;
-
-	bool fetchXml();
-};
-
-class ComponentPanel
-{
-public:
-	vector<Component> components;
-
-	int startPosX;
-	int startPosY;
-
-	int componentHeight = 100;
-	int componentWidth = 100;
-
-	int offsetX;
-	int offsetY;
-
-	bool horizontal = true;
-
-	void push(Component component)
-	{
-		if (component.path != "")
-		{
-			int size = components.size();
-			component.startPosX = startPosX;
-			component.startPosY = startPosY;
-
-			component.component->setHeight(componentHeight);
-			component.component->setWidth(componentWidth, 0);
-
-			component.height = componentHeight;
-			component.width = componentWidth;
-
-			if (size == 0)
-			{
-				component.component->setPosition(startPosX, startPosY);
-
-				cout << "add" << component.startPosX << endl;
-			}
-			else
-			{
-				if (horizontal)
-				{
-					component.startPosX = startPosX + component.width * (size)+offsetX * (size);
-					component.component->setLabel(to_string(size));
-					cout << "add" << component.startPosX << endl;
-				}
-				else
-				{
-					component.startPosY = startPosY * (size + 1) + offsetY * (size);
-				}
-				component.component->setPosition(component.startPosX, component.startPosY);
-			}
-
-			component.currentPosX = component.startPosX;
-			component.currentPosY = component.startPosY;
-
-			/*cout << "start:" << endl;
-			cout << "X = " << component.startPosX << "\nY = " << component.startPosY << endl;
-			cout << "current:" << endl;
-			cout << "X = " << component.currentPosX << "\nY = " << component.currentPosY << endl;*/
-
-			components.push_back(component);
-		}
-	}
-};
 
 ComponentPanel horizontalPanel;
 ComponentPanel verticalPanel;
@@ -231,6 +85,9 @@ void runInCurrentPlayer(MediaViewer* mediaViewer, int res)
 	try {
 		if (horizontalPanel.components[res].type == IMAGE)
 		{
+			//clear video if get image
+			mediaViewer->videoPlayer.stop();
+			//
 			cout << "IMAGE" << endl;
 			mediaViewer->isImageNow = true;
 			auto img = horizontalPanel.components[res].image;
@@ -246,11 +103,10 @@ void runInCurrentPlayer(MediaViewer* mediaViewer, int res)
 
 			auto height = mediaViewer->videoPlayer.getHeight();
 			auto width = mediaViewer->videoPlayer.getWidth();
-			//
+			
 			auto maxHeight = mediaViewer->maxHeight;
 			auto maxWidth = mediaViewer->maxWidth;
-			//
-			//
+
 			if (width > height)
 			{
 				cout << "width>height" << endl;
